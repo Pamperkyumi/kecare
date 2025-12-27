1db661c6b12159a9b4897cb137c804b7d0fe7dea
pamperkyumi <pamperprms2024@gmail.com>
2025-12-27 23:08:30 +0800

    feat: update

--- projects/generator/module-handlers/__EXPORTS__.ts
+++ projects/generator/module-handlers/__EXPORTS__.ts
@@ -1,13 +1,9 @@
 import { articleDetailModuleHandler } from './article-detail';
 import { articleListModuleHandler } from './article-list';
 import { archivesModuleHandler } from './archives';
-import { sidebarModuleHandler } from './sidebar';
-import { docsIndexModuleHandler } from './docs-index';
 
 export const moduleHandlers = {
   'article-detail': articleDetailModuleHandler,
   'article-list': articleListModuleHandler,
   'blog-archives': archivesModuleHandler,
-  'blog-sidebar': sidebarModuleHandler,
-  'docs-index': docsIndexModuleHandler,
 };
